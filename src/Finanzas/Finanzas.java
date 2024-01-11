e010f68d1a6d5a5a29f919a96b328bb29d6a04c3
Daniel Armada González <a23danielag@ATEIS.LOCAL>
2024-01-11 20:47:26 +0100

    Package Finanzas

---
+++ src/Finanzas/Finanzas.java
@@ -0,0 +1,24 @@
+package Finanzas;
+
+public class Finanzas {
+    final double CONVERSION_DOLAR_EURO = 1.36;
+
+    private double dolarEuro;
+
+    public Finanzas(double dolarEuro) {
+        this.dolarEuro = dolarEuro;
+    }
+
+    public Finanzas() {
+        this.dolarEuro = CONVERSION_DOLAR_EURO;
+    }
+
+    public double dolaresToEuros(double dolar){
+        return dolar * dolarEuro;
+    }
+
+    public double eurosToDolares(double euro){
+        return euro / dolarEuro;
+    }
+}
+
---
+++ src/Finanzas/Main.java
@@ -0,0 +1,9 @@
+package Finanzas;
+
+public class Main {
+    public static void main(String[] args) {
+        Finanzas test = new Finanzas(0.4);
+
+        System.out.println(test.dolaresToEuros(1233));
+    }
+}

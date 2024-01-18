ad09dd11581d2cd1a214846f8fc036c66e2ed2b8
Daniel Armada González <a23danielag@ateis.local>
2024-01-18 21:49:11 +0100

    refactor(Universidad/Main):cambia datos de los objetos

--- src/Universidad/Main.java
+++ src/Universidad/Main.java
@@ -11,9 +11,9 @@ public class Main {
         test[1] = new Alumno("Julian", 16, "98233483S", 'N', 73, 1.66, "Biologia Marina", "Biologia Marina");
         test[2] = new AlumnoInternacional("Sarah", 22, "98765432W", 'M', 56, 1.81, "Derecho", "Derecho", "Gales");
         test[3] = new Docente("Paco", 12,"5468454G", 's',78,1.64,"materia");
-        test[4] = new Docente("Paco",100,"12312412G",'O',87,1.89,"Tus muertos");
-        test[5] = new Alumno("Ataulfo", 18,"74878641535F",'G',75,1.85,"me la pela","me la pela aun mas");
-        test[6] = new AlumnoInternacional("Ak47", 23,"5456454F",'H',98,1.95,"me la pela","me la pela aun mas", "Mauritania");
+        test[4] = new Docente("Paco",100,"12312412G",'O',87,1.89,"materia");
+        test[5] = new Alumno("Ataulfo", 18,"74878641535F",'G',75,1.85,"teis","DAW");
+        test[6] = new AlumnoInternacional("Paquito", 23,"5456454F",'H',98,1.95,"teis","DAW", "Mauritania");
 
 
         Arrays.stream(test).forEach(Persona::mostrarInfo);
